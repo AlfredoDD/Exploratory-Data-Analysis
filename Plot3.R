fa4929bac3b029f3a6dddb3d25588d454630ccc8
Alfredo <AlfredoDD@users.noreply.github.com>
2015-04-12 22:23:14 +0200

    Plot3.R
    Rename some variables and remove innecesary libraries

--- Plot3.R
+++ Plot3.R
@@ -5,7 +5,6 @@
 ############################## Environment Preparation ##############################
 # Load necessary packages
 library("data.table")
-library("dplyr")
 # First, create directory to contain data, and fix relative work environment
 if(!file.exists("Project1")){
     dir.create("Project1")
@@ -13,6 +12,7 @@ if(!file.exists("Project1")){
 setwd("Project1")
 
 ######### Read in zip file #########
+######### and extract data to household dataframe #########
 
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 file <- "household_power_consumption"
@@ -41,11 +41,11 @@ rm(householdFeb)
 for(i in c(3:9)) {household[,i] <- as.numeric(household[,i])}
 
 # We use a variable with date an time
-household$DateTime <- paste(household$Date, household$Time)
-household$DateTime <- strptime(household$DateTime, format="%Y-%m-%d %H:%M:%S")
+household$DataHora <- paste(household$Date, household$Time)
+household$DataHora <- strptime(household$DataHora, format="%Y-%m-%d %H:%M:%S")
 
 ######### Plot 3 for the project ###########
-######### Energy Sub Metering vs DateTime #########
+######### Energy Sub Metering vs Date&Time variable #########
 ######### Differents meterings in differents colors #######
 ######### and lines joining this meterings ########
 ######### Put margings(mar) ######
@@ -56,13 +56,13 @@ png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "white
 par(mar = c(7, 6, 5, 4))
 
 
-plot(household$DateTime, household$Sub_metering_1, xaxt=NULL, xlab = "", ylab = "Energy sub metering", type="n")
+plot(household$DataHora, household$Sub_metering_1, xaxt=NULL, xlab = "", ylab = "Energy sub metering", type="n")
 
-lines(household$DateTime, household$Sub_metering_1, col = "black", type = "S")
+lines(household$DataHora, household$Sub_metering_1, col = "black", type = "S")
 
-lines(household$DateTime, household$Sub_metering_2, col = "red", type = "S")
+lines(household$DataHora, household$Sub_metering_2, col = "red", type = "S")
 
-lines(household$DateTime, household$Sub_metering_3, col = "blue", type = "S")
+lines(household$DataHora, household$Sub_metering_3, col = "blue", type = "S")
 
 legend("topright", lty = c(1, 1), lwd = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
